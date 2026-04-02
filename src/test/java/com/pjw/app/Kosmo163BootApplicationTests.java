79184b78faea7e4b093b68541ee88765c1dbb20d
whddns5877 <whddns5877@gmail.com>
2026-04-02 13:07:30 +0900

    2026-04-02

---
+++ src/test/java/com/pjw/app/Kosmo163BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjw.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kosmo163BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
